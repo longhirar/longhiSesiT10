11e1914b9d04412e7c4c8b88999dd95c03e8723c
longhirar <longhirar@gmail.com>
2024-02-28 11:31:54 -0300

    ProfRandom

--- ProfRandom/login.js
+++ ProfRandom/login.js
@@ -13,6 +13,15 @@ contas = [
 ]
 var logado = false;
 
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+
+function login_show() {
+    document.querySelector('.signup-wrap').classList.add('hidden');
+    document.querySelector('.login-wrap').classList.remove('hidden');
+    document.querySelector('.logged-in').classList.add('hidden');
+}
+
 function login() {
     input_email = document.querySelector('#input-email').value;
     input_pass = document.querySelector('#input-password').value;
@@ -36,4 +45,51 @@ function logout() {
     document.querySelector('.login-wrap').classList.remove('hidden');
     document.querySelector('.logged-in').classList.add('hidden');
     logado = false;
+}
+
+function signup_show() {
+    document.querySelector('.signup-wrap').classList.remove('hidden');
+    document.querySelector('.login-wrap').classList.add('hidden');
+    document.querySelector('.logged-in').classList.add('hidden');
+}
+
+function signup() {
+    var input_name = document.querySelector('#input-name-signup').value;
+    var input_email = document.querySelector('#input-email-signup').value;
+    var input_pass =  document.querySelector('#input-password-signup').value;
+    var input_pass2 =  document.querySelector('#input-password-2-signup').value;
+    
+    contas.forEach(conta => {
+        if (conta.email === input_email) {
+            alert("Uma conta já existe com esse email.")
+            return;
+        } 
+    })
+
+    if (input_pass != input_pass2) {
+        alert("As duas senhas precisam ser iguais!");
+        return;
+    }
+
+    if (!(/\d/.test(input_pass))) {
+        alert("Sua senha não atende aos requisitos: Sua senha precisa ter pelo menos 1 número!");
+        return;
+    }
+    if (!(input_pass.length >= 8)) {
+        alert("Sua senha não atende aos requisitos: Sua senha precisa ter pelo menos 8 caractéres!");
+        return;
+    }
+    if (!(/[A-Z]/.test(input_pass))) {
+        alert("Sua senha não atende aos requisitos: Sua senha precisa ter uma letra maiúscula!");
+        return;
+    }
+
+
+    contas.push({
+        name: input_name,
+        email: input_email,
+        pass: input_pass,
+    })
+
+    login_show();
 }
