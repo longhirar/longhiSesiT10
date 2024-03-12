5255b10cecee048dff192663aadec0e65c839a08
longhirar <longhirar@gmail.com>
2024-03-12 09:39:24 -0300

    TabletoExcel 12.03

--- TabletoExcel/script.js
+++ TabletoExcel/script.js
@@ -1,21 +1,35 @@
 
+var TABELA = []
+
 const adicionarItem = () => {
-    var nome = document.querySelector('#input-nome').value;
-    var valor = document.querySelector('#input-valor').valueAsNumber;
-    var qtd = document.querySelector('#input-qtd').valueAsNumber;
+    var nome = document.querySelector('#input-nome');
+    var valor = document.querySelector('#input-valor');
+    var qtd = document.querySelector('#input-qtd');
 
     console.log(nome, valor, qtd);
 
     var tr = document.createElement('tr');
     var tdnome = document.createElement('td');
-    tdnome.textContent = nome;
+    tdnome.textContent = nome.value;
     tr.appendChild(tdnome);
     var tdvalor = document.createElement('td');
-    tdvalor.textContent = valor + "Dabloons";
+    tdvalor.textContent = valor.valueAsNumber + "Dabloons";
     tr.appendChild(tdvalor);
     var tdqtd = document.createElement('td');
-    tdqtd.textContent = qtd;
+    tdqtd.textContent = qtd.valueAsNumber;
     tr.appendChild(tdqtd);
     
-    document.querySelector('.tabela').appendChild(tr);
+    document.querySelector('#tabela').appendChild(tr);
+
+    TABELA.push([nome.value, valor.valueAsNumber, qtd.valueAsNumber]);
+    console.log(TABELA);
+
+
+}
+
+const criarXlsx = () => {
+    var tabela = document.querySelector('#tabela');
+    var nomeArquivo = "tabela.xlsx";
+    var wb = XLSX.utils.table_to_book(tabela, {sheet: "Tabela"});
+    XLSX.writeFile(wb, nomeArquivo);
 }
