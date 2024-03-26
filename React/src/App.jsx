7eb91fdb342ce40646873d8aa25a33fd8115339a
longhirar <longhirar@gmail.com>
2024-03-26 12:28:36 -0300

    React 26.03-2

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,18 +1,18 @@
-import './App.css'
+import style from './App.module.css'
 import Card from './components/Card'
 
 import img01 from "./assets/imgs/foto1.jpg";
 import img02 from "./assets/imgs/foto2.jpg";
 import img03 from "./assets/imgs/foto3.jpg";
 
-function App() {
+import { Tilt } from 'react-tilt'
+
+export default function App() {
   return (
-    <>
+    <div className={style.section01}>
       <Card title="Pessoa!" desc="Uma pessoa muito legal" price={32.99} img={img01} />
       <Card title="Floresta!" desc="Um bom passeio" price={6.99} img={img02} />
       <Card title="Matagal!" desc="Só quem pode fumar no milharal" price={32.99} img={img03} />
-    </>
+    </div>
   )
 }
-
-export default App
